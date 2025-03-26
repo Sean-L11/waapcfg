18dc1ed483e4c67bd3bee64b0a68ceeaa5449f6c
sean <s.power@link11.com>
2025-03-26 07:43:06 -0400

    origin service

--- cfg5/src/app/app.component.ts
+++ cfg5/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import {FormGroup, FormControl} from '@angular/forms';
-import {ReactiveFormsModule} from '@angular/forms';
+import {ReactiveFormsModule, Validators} from '@angular/forms';
+import { OriginService } from './origin.service';
 
 
 @Component({
@@ -14,13 +15,15 @@ import {ReactiveFormsModule} from '@angular/forms';
 export class AppComponent {
   title = 'cfg5';
   apikey = 'L7rOxY78SK2-XpL7dsGipWcCJ818DRWE6xGfjB2eiheQcjLnEHUxXfqrGil9K405';
+  backend = inject(OriginService);
   websiteForm = new FormGroup({
-    domain: new FormControl(''),
-    origin: new FormControl(''),
+    domain: new FormControl('', [Validators.required, Validators.pattern('.*')]),
+    origin: new FormControl('', [Validators.required, Validators.pattern('.*')]),
   })
 
 
   getConfig() {}
 
-  submitConfig() {}  
+  submitConfig() {
+  }  
 }
