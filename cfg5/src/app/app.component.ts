91239cf14c7d162096261ea7505c92366fc16679
sean <s.power@link11.com>
2025-03-26 07:04:35 -0400

    reactive forms

--- cfg5/src/app/app.component.ts
+++ cfg5/src/app/app.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import {FormGroup, FormControl} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, RouterOutlet],
+  imports: [FormsModule, ReactiveFormsModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'cfg5';
-  apikey = '';
+  apikey = 'L7rOxY78SK2-XpL7dsGipWcCJ818DRWE6xGfjB2eiheQcjLnEHUxXfqrGil9K405';
+  websiteForm = new FormGroup({
+    domain: new FormControl(''),
+    origin: new FormControl(''),
+  })
+
 
   getConfig() {}
-  
+
+  submitConfig() {}  
 }
