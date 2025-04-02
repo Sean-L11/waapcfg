438b3f3b846d66c5c08149501cae450bc0ba7c5b
sean <s.power@link11.com>
2025-04-02 14:29:24 -0400

    security policy

---
+++ cfg5/src/app/securitypolicy.ts
@@ -0,0 +1,57 @@
+export interface SecurityPolicyInterface {
+	id: string;
+	name: string;
+	session: [any];
+	tags: [];
+	map: any;
+}
+
+export interface PathMapInterface {
+	id: string;
+	name: string;
+	match: string;
+	acl_profile: string;
+	acl_profile_active: boolean;
+	content_filter_profile: string;
+	content_filter_profile_active: boolean;
+	backend_service: string;
+	edge_functions: [];
+	rate_limit_functions: [];
+}
+
+export class SecurityPolicy implements SecurityPolicyInterface {
+	id: string = '';
+	name: string = '';
+	session: [any] = [{"attr" : "ip"}]
+	tags: [] = [];
+	map: any;
+	
+	constructor(backend: string){
+		let root = new PathMap('__root_entry__', '^/(\\W.*)?$', backend);
+		let site = new PathMap('__site_level__', '__site_level__', backend);
+		let dflt = new PathMap('__default_entry__', '/', backend);
+	
+		this.map = [root, site, dflt];	
+
+	}
+}
+
+export class PathMap implements PathMapInterface {
+	id: string;
+	name: string;
+	match: string;
+	acl_profile: string = '__acl_default__';
+	acl_profile_active: boolean = false;
+	content_filter_profile: string = '__defaultcontentfilter__';
+	content_filter_profile_active: boolean = false;
+	backend_service: string;
+	edge_functions: [] = [];
+	rate_limit_functions: [] = [];
+
+	constructor(id: string, path: string, backend: string){
+		this.id = id;
+		this.name = path.replace(/__/gi, '');
+		this.match = path;
+		this.backend_service = backend;
+	}
+}
